0f800eaa4690e2965874a5be988ea30fbdf85955
dyremm <beganbajrami@gmail.com>
2021-12-23 17:22:58 +0100

    intial testings

--- rplugin/python3/vim_icue.py
+++ rplugin/python3/vim_icue.py
@@ -7,9 +7,11 @@ class VimICUE(object):
     def __init__(self, vim:neovim.Nvim):
         logging.basicConfig(level=logging.DEBUG)
         self.vim = vim
+        self.insertModeOn = self.vim.subscribe("InsertEnter")
+        self.insertModeOff = self.vim.subscribe("InsertEnter")
 
-    @neovim.function('DoItPython', sync=False)
-    def get_mode(self, args):
+    @neovim.function('VimICUEGetMode', sync=False)
+    def getMode(self, args):
        mode = self.vim.call('mode')
        self.vim.current.line = mode
 
