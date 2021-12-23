6a96c6014891f57725e36516c1c8f31ec1a03313
dyremm <beganbajrami@gmail.com>
2021-12-23 17:46:31 +0100

    intial testings

--- rplugin/python3/vim_icue.py
+++ rplugin/python3/vim_icue.py
@@ -7,7 +7,7 @@ class VimICUE(object):
     def __init__(self, vim: neovim.Nvim):
         #logging.basicConfig(level=logging.DEBUG)
         self.vim = vim
-        self.vim.command("echomsg 'vim-icue is initializing...'")
+        self.vim.call("echomsg 'vim-icue is initializing...'")
         self.cue = CueSdk()
         self.connected = self.cue.connect()
         if not self.connected:
@@ -19,7 +19,7 @@ class VimICUE(object):
 
         self.insert_mode_on = self.vim.subscribe("InsertEnter")
         self.insert_mode_off = self.vim.subscribe("InsertLeave")
-        self.vim.command("echomsg 'vim-icue is ready!'")
+        self.vim.call("echomsg 'vim-icue is ready!'")
 
     def get_available_leds(self):
         leds = list()
